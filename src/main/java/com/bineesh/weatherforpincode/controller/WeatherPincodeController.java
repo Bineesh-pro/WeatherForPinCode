2dda5ac8cd79cf5c7fc1d11ae0d0e955ec52a06b
Bineesh-pro <bineeshprofessional@gmail.com>
2024-10-17 16:05:04 +0530

    rest end point added

---
+++ src/main/java/com/bineesh/weatherforpincode/controller/WeatherPincodeController.java
@@ -0,0 +1,28 @@
+package com.bineesh.weatherforpincode.controller;
+
+
+import com.bineesh.weatherforpincode.service.WeatherInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class WeatherPincodeController {
+
+    @Autowired
+    WeatherInfoService weatherInfoService;
+
+
+    @GetMapping("weather-info")
+    public ResponseEntity<String> getWeatherInfo(
+            @RequestParam String pincode,
+            @RequestParam @DateTimeFormat(pattern = "dd/MM/yyyy") Date for_date
+   ){
+        return ResponseEntity.ok(weatherInfoService.getWeatherInfo(pincode,for_date));
+    }
+}
---
+++ src/main/java/com/bineesh/weatherforpincode/service/WeatherInfoService.java
@@ -0,0 +1,13 @@
+package com.bineesh.weatherforpincode.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class WeatherInfoService {
+
+    public String getWeatherInfo(String pincode, Date date){
+        return "hi";
+    }
+}
